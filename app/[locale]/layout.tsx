f86ca5ece2400c39c8afce4cfdf6e759f676f840
Olimjon Gulomov <olimjongulomov320@gmail.com>
2026-01-21 16:34:24 +0500

    Initial version of academic website

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,24 @@
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { notFound } from "next/navigation";
+import { locales } from "@/next-intl.config";
+
+export default async function LocaleLayout({
+  children,
+  params
+}: {
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+
+  if (!locales.includes(locale as any)) notFound();
+
+  const messages = await getMessages();
+
+  return (
+    <NextIntlClientProvider locale={locale} messages={messages}>
+      {children}
+    </NextIntlClientProvider>
+  );
+}
